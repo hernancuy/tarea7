b3fe02856b13462c7f8324c67da1865f46c8948f
Arturo Hurtado <ja.hurtado905@uniandes.edu.co>
2022-04-24 18:54:46 -0500

    image update

--- project/lib/project.ex
+++ project/lib/project.ex
@@ -6,12 +6,6 @@ defmodule Project do
   def init(path) do
     {_valor, _status} = System.cmd("cmd.exe",["/c","python","lib/image.py",path])
     {:ok, data} = File.read("image.txt")
-    IO.puts("Output")
-    #IO.inspect(data)
-    #String.split(data, ~r/.{3}/, include_captures: true, trim: true)
-    #pixels = :binary.bin_to_list(data)
-    #IO.inspect(pixels)
-    #image = data |> Kernel.to_string(pixels)
 
     #Project.init("C:/Users/ArturoHurtado/Downloads/descargar.png")
     image = data
@@ -22,10 +16,8 @@ defmodule Project do
 
     len = image
     |> length()
-    |> IO.inspect()
 
     last = image
-    |> IO.inspect()
     |> Enum.at(len-1)
   end
 
